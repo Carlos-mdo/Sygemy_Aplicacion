f05d4aa580063687dc14e6004a66f9cfd030bd92
Valentino Nuñez <valentino3279@gmail.com>
2026-03-31 16:03:01 -0300

    Implementacion de detalles Admin

--- app/src/main/java/Servicios/ServicioAdmin.java
+++ app/src/main/java/Servicios/ServicioAdmin.java
@@ -7,40 +7,74 @@ import android.database.sqlite.SQLiteDatabase;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import Datos.AdminSQLiteOpenHelper;
+import Entidades.Usuarios;
 
 public class ServicioAdmin {
-//    protected Context context;
-//    protected boolean estado;
-//    private AdminSQLiteOpenHelper admin;
-//    private SQLiteDatabase base_Datos;
-//
-//    public ServicioAdmin(Context context){
-//        this.context = context;
-//        admin = new AdminSQLiteOpenHelper(context, "BD_Sygemy", null, 1);
-//        base_Datos = admin.getWritableDatabase();
-//    }
-//
-//    public boolean buscarAdmin(String etUsuario, String etContrasenia) {
-//
-//        Cursor usuarAdmin = base_Datos.rawQuery("select usuario from usuarios", null);
-//        Cursor contraAdmin = base_Datos.rawQuery("select contrasenia from usuarios",null);
-//
-//        if (usuarAdmin.moveToFirst()) {
-//            if(contraAdmin.moveToFirst()){
-//
-//                if (etUsuario.equals(usuarAdmin.getString(0)) && etContrasenia.equals(contraAdmin.getString(0))) {
-//                    estado = true;
-//                }
-//            }
-//        } else {
-//            Toast.makeText(context, "no existen datos de usuario en la tabla", Toast.LENGTH_SHORT).show();
-//            estado = false;
-//        }
-//
-//        usuarAdmin.close();
-//        contraAdmin.close();
-//
-//        return estado;
-//    }
+        protected Context context;
+        protected boolean estado;
+        private AdminSQLiteOpenHelper admin;
+        private SQLiteDatabase base_Datos;
+
+        public ServicioAdmin(Context context){
+            this.context = context;
+            admin = new AdminSQLiteOpenHelper(context, "BD_Sygemy", null, 1);
+            base_Datos = admin.getWritableDatabase();
+        }
+
+        public boolean buscarAdmin(String etUsuario, String etContrasenia) {
+
+        Cursor cursor = base_Datos.rawQuery(
+                "SELECT * FROM usuarios WHERE usuario=? AND contrasenia=?",
+                new String[]{etUsuario, etContrasenia}
+        );
+
+        boolean existe = cursor.moveToFirst();
+
+        cursor.close();
+
+        return existe;
+        }
+    public List<Usuarios> BuscarTodosUsuarios() {
+
+        List<Usuarios> listaUsuarios = new ArrayList<>();
+
+        Cursor cursor = base_Datos.rawQuery("SELECT * FROM usuarios", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+                String nombreUsuario = cursor.getString(cursor.getColumnIndexOrThrow("usuario"));
+                String contrasenia = cursor.getString(cursor.getColumnIndexOrThrow("contrasenia"));
+                String rol = cursor.getString(cursor.getColumnIndexOrThrow("rol"));
+
+                Usuarios usuario = new Usuarios(id,nombreUsuario, contrasenia, rol);
+                listaUsuarios.add(usuario);
+
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+
+        return listaUsuarios;
+    }
+    public void eliminarUsuario(int id) {
+        base_Datos.delete("usuarios", "id = ?",
+                new String[]{String.valueOf(id)});
+    }
+
+    public void actualizarUsuario(Usuarios usuario) {
+
+        ContentValues valores = new ContentValues();
+        valores.put("usuario", usuario.getUsuario());
+        valores.put("contrasenia", usuario.getContrasenia());
+        valores.put("rol", usuario.getRol());
+
+        base_Datos.update("usuarios", valores, "id = ?",
+                new String[]{String.valueOf(usuario.getId())});
+    }
 }
